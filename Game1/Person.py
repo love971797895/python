6fa0039c690e64eb3fa77be05569a324f411753e
Jeff <qq11121119@163.com>
2017-07-23 20:57:13 +0800

    基础设施

---
+++ Game1/Person.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python  
+# -*- coding: <utf-8> -*- 
+class Person(object):
+	"""docstring for ClassName"""
+	def __init__(self, name):
+		super(ClassName, self).__init__()
+		self.name = name
+		
