638551385a649bd26a6e1f414554ec4e8697d3e5
Jeff.Mr <qq11121119@sina.com>
2017-07-24 18:09:06 +0800

    添加初始化基类

--- Game1/main.py
+++ Game1/main.py
@@ -1,5 +1,39 @@
 #!/usr/bin/python  
 # -*- coding: <utf-8> -*- 
+
+class SpearInfo(object):
+	"""docstring for Spear"""
+	def __init__(self, name):
+		super(Spear, self).__init__()
+		self.name = name
+
+
+class SpearClip(object):
+	"""docstring for SpearClip"""
+	def __init__(self, name):
+		super(SpearClip, self).__init__()
+		self.name = name
+
+
+class SpearBullet(object):
+	"""docstring for SpearBullet"""
+	def __init__(self, arg):
+		super(SpearBullet, self).__init__()
+		self.arg = arg
+		
+
+
+class Person(object):
+	"""docstring for Person"""
+	def __init__(self, arg):
+		super(Person, self).__init__()
+		self.arg = arg
+
+
+		
+
+
+
 def Main():
 	pass
 
