21014550f57048e1a45dff85940e8317c685359d
Jeff.Mr <qq11121119@sina.com>
2017-07-26 18:56:13 +0800

    添加初始化界面

--- Game2/Main.py
+++ Game2/Main.py
@@ -1,2 +1,39 @@
 #!/usr/bin/python  
 # -*- coding: <utf-8> -*- 
+import pygame,os,sys,time
+
+""" python 3.+的版本中，print需要用 () 
+	# 获取当前文件__file__的路径
+    print "os.path.realpath(__file__)=%s" % os.path.realpath(__file__)
+    # 获取当前文件__file__的所在目录
+    print "os.path.dirname(os.path.realpath(__file__))=%s" % os.path.dirname(os.path.realpath(__file__)) 　　
+    # 获取当前文件__file__的所在目录
+    print "os.path.split(os.path.realpath(__file__))=%s" % os.path.split(os.path.realpath(__file__))[0]　
+"""
+
+try:
+	def main():
+		screen = pygame.display.set_mode((482,854),0,32)
+		osurl = os.path.dirname(os.path.realpath(__file__))
+
+		backurl = osurl + "\\content\\back.png" #背景图片的存放地址
+		obackground = pygame.image.load(backurl)
+
+		mplane = osurl + "\\content\\mplane.png" #要打的飞机的存放地址
+		omplane = pygame.image.load(mplane)
+		
+		while True:
+			screen.blit(obackground,(0,0))
+			screen.blit(omplane,(0,0))
+			pygame.display.update()
+			time.sleep(0.01) #延时执行
+except Exception as e:
+	print(e)
+else:
+	pass
+finally:
+	pass
+
+
+if __name__ == '__main__':
+	main()
